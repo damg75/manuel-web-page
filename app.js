34c7e695d8105d16100ce25b69bea40adfab6a73
Daniel Moran <damg75cd@gmail.com>
2021-03-24 22:49:05 -0400

    page layout done

--- app.js
+++ app.js
@@ -16,3 +16,18 @@ function scrollFunction() {
 mybutton.addEventListener("click", function scroller() {
     window.scroll({top: 0, left: 0, behavior: 'smooth'});
 });
+
+document.getElementById("flexRadioDefaultbusiness").onchange = function() {
+    var input_empresa = document.getElementById("input_business");
+    input_empresa.style.display = "block";
+    document.getElementById("flexRadioDefaultperson").checked = false
+};
+document.getElementById("flexRadioDefaultperson").onchange = function() {
+    var input_empresa = document.getElementById("input_business");
+    input_empresa.style.display = "none";
+    document.getElementById("flexRadioDefaultbusiness").checked = false;
+    document.getElementById("input_business_input").value = "";
+};
+
+current_year = document.getElementById("current-year-footer");
+current_year.innerHTML = new Date().getFullYear();
